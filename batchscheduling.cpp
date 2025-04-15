aeb2019f5c5b0f3c07bf3d08f904292cd4156223
ogbidaniel <ogbuigwed@gmail.com>
2025-04-15 14:49:11 -0500

    init batchscheduling

---
+++ batchscheduling.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <numeric>
+
+using namespace std;
+
+// Process structure
+struct Process
+{
+
+};
+
+
+// --- Simulation Core Logic (Common parts refactored if desired, or separate for clarity) ---
+
+// --- FIFO Simulation ---
+
+// --- SJF Simulation ---
+
+// --- SRT Simulation ---
+
+
+int main() {
+    // Simulation Parameters
+
+    
+}
